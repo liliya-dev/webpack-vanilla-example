23a885b5b9b0a621bed566c05de94189d66d8989
Yurii Holiuk <bionicuss@gmail.com>
2020-07-30 11:44:04 +0300

    WIP

--- src/index.js
+++ src/index.js
@@ -1,21 +1,117 @@
-import { hello, tes } from './scripts/import-example';
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
 
-import './styles/style.css';
+// // const controller = new ScrollMagic.Controller();
+// const tween = new TimelineLite();
+// const flightPath = {
+//   curviness: 1.25,
+//   autoRotate: true,
+//   values: [
+//     {
+//       x: 600, y: -20,
+//     },
+//     {
+//       x: 100, y: -200,
+//     },
+//     {
+//       x: -100, y: -300,
+//     },
+
+//   ],
+// };
+
+// tween.addTo(
+//   TweenLite.to('.fly', {
+//     duration: 1,
+//     Bezier: flightPath,
+//   }),
+// );
+
+// console.log(tween);
+// new ScrollMagic.Scene({
+//   duration: 100, // the scene should last for a scroll distance of 100px
+//   offset: 50, // start this scene after scrolling for 50px
+// })
+//   .setPin('#header') // pins the element for the the scene's duration
+//   .addTo(controller);
+
 import './styles/style.scss';
-import './styles/style.sass';
 
-hello();
+gsap.registerPlugin(MotionPathPlugin);
 
-async function run() {
-  const value = await tes();
-  console.log(value)
+for (let i = 1; i < 7; i = i + 2) {
+  const tween = gsap.timeline();
+
+  tween.to(`.header-img${i}`, {
+    repeat: -1,
+    yoyo: true,
+    // animationDirection: 'power1.alternate',
+    duration: 7,
+    delay: 1,
+    ease: 'power1.ease-in-out',
+    motionPath: {
+      path: [{
+        x: -20, y: -10,
+      }],
+      curviness: 2,
+      // autoRotate: true,
+    },
+  });
 }
 
-run();
+for (let i = 2; i < 7; i = i + 2) {
+  const tween = gsap.timeline();
 
-async function lazyLoadExample() {
-  const { lazyLoad } = await import('./scripts/lazy-load-example');
-  lazyLoad().then(res => console.log(res));
-};
+  tween.to(`.header-img${i}`, {
+    repeat: -1,
+    yoyo: true,
+    // animationDirection: 'power1.alternate',
+    duration: 7,
+    delay: 1,
+    ease: 'power1.ease-in-out',
+    motionPath: {
+      path: [{
+        x: 30, y: 20,
+      }],
+      curviness: 2,
+      // autoRotate: true,
+    },
+  });
+}
 
-document.querySelector("#lazy-load").addEventListener('click', lazyLoadExample);
+const headerCenter = document.querySelector('.header__center');
+const loading = document.querySelector('.loading');
+const loadingText = document.querySelector('.loading__text');
+const loadingImg = document.querySelector('.loading__img');
+
+headerCenter.addEventListener('click', function() {
+  hidePictures();
+  changeNumbers();
+});
+
+function hidePictures() {
+  const images = document.querySelectorAll(`.header-img`);
+
+  for (const img of images) {
+    img.style.display = 'none';
+  }
+}
+
+loadingImg.addEventListener('click', function() {
+  loadingImg.src = './images/loading/loading1.png';
+});
+
+function changeNumbers() {
+  loading.style.display = 'block';
+
+  let number = 1;
+
+  const timerNumber = setInterval(() => {
+    number++;
+
+    if (number === 100) {
+      clearInterval(timerNumber);
+    }
+    loadingText.textContent = number;
+  }, 20);
+}
