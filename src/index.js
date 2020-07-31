6c45cf02de0e6c9da805dbabe937c3089f8ed7bc
Liliia <chuiko1112@gmail.com>
2020-07-31 22:20:46 +0300

    60%

--- src/index.js
+++ src/index.js
@@ -57,19 +57,17 @@ headerCenter.addEventListener('click', startLoadingListener, false);
 const controller = new ScrollMagic.Controller({
   globalSceneOptions: {
     triggerHook: 'onLeave',
-    duration: '100%',
+    // duration: '100%',
   },
 });
 
 const slides = document.querySelectorAll('.slider');
-console.log(slides)
 
 for (let i = 0; i < slides.length; i++) {
   new ScrollMagic.Scene({
     triggerElement: slides[i],
   })
     .setPin(slides[i], { pushFollowers: false })
-    .addIndicators()
     .addTo(controller);
 }
 
@@ -80,11 +78,11 @@ const mainText = document.querySelector('.wedo__name');
 
 const texts = {
   1: 'Channel Operation and Management',
-  2: 'Creative Services for Infuencers',
-  3: 'Graphic Design & Photography',
-  4: 'Lyric Videos & Animation',
-  5: 'Business Development',
-  6: 'Brand Campaigns',
+  2: 'Creative Services  \n for Infuencers',
+  3: 'Graphic Design \n & Photography',
+  4: 'Lyric Videos \n & Animation',
+  5: 'Business \n Development',
+  6: 'Business \n Development',
 };
 
 wedo.addEventListener('click', (event) => {
@@ -153,7 +151,6 @@ wedo.addEventListener('mouseover', () => {
 
   for (let i = 1; i < 4; i = i + 1) {
     const tween = gsap.timeline();
-    // const smallImg = document.querySelector(`.wedo__small-img--${i}`);
 
     tween.to(`.wedo__small-img--${i}`, {
       repeat: 1,
@@ -166,4 +163,37 @@ wedo.addEventListener('mouseover', () => {
       },
     });
   }
+});
+
+const headerNumber = document.querySelector('.header__number');
+
+function increaseNumber() {
+  const currentTextContent = headerNumber.textContent;
+  const currentNumber = Number(currentTextContent.split(' ').join(''));
+  const nextNumber = currentNumber + 1;
+  const nextNumberString = nextNumber + '';
+  let nextTextContent = '';
+  let space = 0;
+
+  for (let i = nextNumberString.length - 1; i >= 0; i--) {
+    if (space === 3) {
+      nextTextContent = ' ' + nextTextContent;
+      space = 0;
+    }
+
+    nextTextContent = nextNumberString.charAt(i) + nextTextContent;
+    space++;
+  };
+
+  headerNumber.textContent = nextTextContent;
+}
+
+setInterval(increaseNumber, 30);
+
+const mainDoc = document.querySelector('html');
+const cursor = document.querySelector('.cursor');
+
+mainDoc.addEventListener('mousemove', (event) => {
+  cursor.style.left = event.pageX + 'px';
+  cursor.style.top = event.pageY + 'px';
 });
